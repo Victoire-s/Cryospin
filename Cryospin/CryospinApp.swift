b0283b44d11c75d4b0f7a6d65e69b04b64c2c9a0
Victoires <94651953+Victoire-s@users.noreply.github.com>
2026-03-02 15:39:44 +0100

    Initial Commit

---
+++ Cryospin/CryospinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryospinApp.swift
+//  Cryospin
+//
+//  Created by Tristan on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CryospinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
